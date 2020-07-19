9cba89d5e4e925e38f3ce94b83de287311f15c7f
keys4words <keys4words@gmail.com>
2020-07-19 21:36:26 +0300

    add forms validation

--- looplab/home/forms.py
+++ looplab/home/forms.py
@@ -1,6 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, validators, ValidationError
+import email_validator
 
 class LandingForm(FlaskForm):
-    full_name = StringField('Full name')
-    email = StringField('Email')
+    full_name = StringField('Full name', validators=[validators.DataRequired(message='You need to input your full name')])
+    email = StringField('Email', validators=[validators.DataRequired(
+        message='You need to input your email name'),
+        validators.Email()])
+
+    def validate_email(self, field):
+        if field.data.endswith(".edu"):
+            raise ValidationError('You cannot use school address for your email')

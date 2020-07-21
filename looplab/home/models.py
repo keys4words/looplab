001e1096c3728ee857d7903bb4977af16c4924ef
keys4words <keys4words@gmail.com>
2020-07-21 09:15:31 +0300

    add sqlite3 db and models

---
+++ looplab/home/models.py
@@ -0,0 +1,36 @@
+from looplab import db
+
+
+class EmailSignup(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120), nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def save(self, commit=True):
+        if commit:
+            instance = self
+            if not instance.id:
+                db.session.add(instance)
+            try:
+                db.session.commit()
+            except Exception as e:
+                print("Exception occured\n", e, '\n')
+                db.session.rollback()
+                return False
+            return True
+        return False
+
+    def delete(self, commit=True):
+        if self.id:
+            db.session.delete(self)
+            try:
+                db.session.commit()
+            except Exception as e:
+                print("Exception occured\n", e, '\n')
+                db.session.rollback()
+                return False
+            return True
+        return False
+
+    def __str__(self):
+        return f"{self.id}: {self.full_name} with email {self.email}"

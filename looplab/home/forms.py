937ea8cc20286b967aaafa6ed353495942952e86
keys4words <keys4words@gmail.com>
2020-07-22 09:47:25 +0300

    add detail page and update func

--- looplab/home/forms.py
+++ looplab/home/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, validators, ValidationError
 import email_validator
 
+from .models import EmailSignup
+
 class LandingForm(FlaskForm):
     full_name = StringField('Full name',
         render_kw={"class": "form-control", "placeholder": 'Full name'},
@@ -14,3 +16,7 @@ class LandingForm(FlaskForm):
     def validate_email(self, field):
         if field.data.endswith(".edu"):
             raise ValidationError('You cannot use school address for your email')
+        obj = EmailSignup.query.filter_by(email=field.data).first()
+        if obj is not None:
+            msg = 'This email has already been added!'
+            raise ValidationError(msg)
--- looplab/home/views.py
+++ looplab/home/views.py
@@ -1,11 +1,46 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
+
 from looplab import app
+
 from .forms import LandingForm
+from .models import EmailSignup
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = LandingForm()
     if form.validate_on_submit():
-        print(form.full_name.data)
-        print(form.email.data)
+        data = form.data
+        print(form.data)
+        if 'csrf_token' in data:
+            del data['csrf_token']
+        obj = EmailSignup.query.filter_by(email=form.email.data).first()
+        if obj is None:
+            obj = EmailSignup(**data)
+            obj.save()
+        return redirect('/item/{}'.format(obj.id))
     return render_template('index.html', form=form)
+
+
+@app.route('/item/<int:id>/', methods=['GET'])
+def item_detail(id):
+    # instance = EmailSignup.query.get(id)
+    instance = EmailSignup.query.filter_by(id=id).first_or_404()
+    return render_template('items/detail.html', instance=instance)
+
+
+@app.route('/item/<int:id>/update/', methods=['GET', 'POST'])
+def item_update(id):
+    # instance = EmailSignup.query.get(id)
+    instance = EmailSignup.query.filter_by(id=id).first_or_404()
+    form = LandingForm(obj=instance)
+    if form.validate_on_submit():
+        data = form.data
+        print(data)     
+    return render_template('items/form.html', instance=instance, form=form)
+
+
+@app.route('/item/<int:id>/delete/', methods=['GET', 'POST'])
+def item_delete(id):
+    # instance = EmailSignup.query.get(id)
+    instance = EmailSignup.query.filter_by(id=id).first_or_404()
+    return render_template('items/detail.html', instance=instance)
